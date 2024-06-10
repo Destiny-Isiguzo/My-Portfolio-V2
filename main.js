dc87f4a9d5b58c7e51a549ed60ff44d9d2af3d82
Des19 <petrovichalexander411@gmail.com>
2024-06-10 22:25:31 +0300

    Made some updates

--- main.js
+++ main.js
@@ -100,3 +100,33 @@ const scrollActive = () => {
    })
 }
 window.addEventListener('scroll', scrollActive);
+
+
+// Dark light theme toggle
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-fill';
+
+// previously selected theme (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// we obtain the current theme of the site by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-fill' : 'ri-sun-fill';
+
+// we validate if the user previously chose a theme
+if (selectedTheme) {
+   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+   themeButton.classList[selectedIcon === 'ri-moon-fill' ? 'add' : 'remove'](iconTheme);
+}
+
+// activate & deactivate the theme manually on click
+themeButton.addEventListener('click', () => {
+   // add or remove the dark - icon theme
+   document.body.classList.toggle(darkTheme);
+   themeButton.classList.toggle(iconTheme);
+   // we save the theme and the current icon
+   localStorage.setItem('selected-theme', getCurrentTheme());
+   localStorage.setItem('selected-icon', getCurrentIcon());
+})
